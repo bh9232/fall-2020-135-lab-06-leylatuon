d73e93238061c46b641de36b364634917e579fa6
leylatuon <leylatuon@users.noreply.github.com>
2020-10-09 04:37:46 -0400

    added decrypt and tests

---
+++ decrypt.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cctype>
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
+
+using std::string;
+
+string decryptCaesar(string ciphertext, int rshift)
+{
+    int shifter=(-1)*rshift;
+    return encryptCaesar(ciphertext,shifter);
+}
+string decryptVigenere(string ciphertext, string keyword)
+{
+    int shifter=0, charcounter=0;
+    char chartemp; //character of keyword
+    string result="";
+    int keywordat;
+    for(int i=0;i<ciphertext.length();i++)
+    {
+        if(isalpha(ciphertext[i]))
+        {
+            charcounter++;
+            keywordat=charcounter%keyword.length()-1;
+            if ((int)keywordat<0)
+            {
+                keywordat+=keyword.length();
+            }
+           // cout<<keywordat<<endl;
+            chartemp=keyword.at(keywordat);
+            if(isupper(chartemp))
+            {
+                shifter=(int)chartemp-65;
+            }
+            else if(islower(chartemp))
+            {
+                shifter=(int)chartemp-97;
+            }
+        }
+        shifter=(-1)*(shifter);
+        result+=shiftChar(ciphertext[i],shifter);
+    }
+    return result;
+}
+
+
+
+
